eed4e93a88bb49978866e75f000dd1072bf3b24e
ipuxin <ipuxin@qq.com>
2016-10-18 07:31:11 +0800

    空格编码为了成了下划线

--- md5.php
+++ md5.php
@@ -6,11 +6,14 @@
 
 /**
  * 如果用户名为:dabao&king
- * Array ( [username] => dabao [king] => [age] => 18 )
+ * Array ( [user_name] => dabao& king [age] => 18 )
  */
 if(isset($_GET)){
     print_r($_GET);
 }
 echo '<hr>';
-$str = 'http://localhost/git/php_encrypt/md5.php?username=dabao&king&age=18';
+$strName = urlencode('dabao& king');
+$str = 'http://localhost/git/php_encrypt/md5.php?user name='.$strName.'&age=18';
 echo "<a href='" . $str . "'>特殊用户名</a><br>";
+//http://localhost/git/php_encrypt/md5.php?user%20name=dabao%26%20king&age=18
+//http://localhost/git/php_encrypt/md5.php?user%20name=dabao%26+king&age=18
